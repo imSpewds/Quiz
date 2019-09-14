dabd11805f1c6fae491b85badc4d615737a136a0
BuildTools <unconfigured@null.spigotmc.org>
2019-09-14 11:04:31 -0700

    Finished js

--- script.js
+++ script.js
@@ -1,5 +1,42 @@
-$(document).ready(function() {
-	$(".f1").click(function(){
-		$(".f1").toggle();
+$(document).ready(function(){
+	$(".f1, .t1").click(function(){
+		$(".t1").css("color", "green");
+		$(".f1").css("color", "red");
+	});
+	$(".f2, .t2").click(function(){
+		$(".t2").css("color", "green");
+		$(".f2").css("color", "red");
+	});
+	$(".f3, .t3").click(function(){
+		$(".t3").css("color", "green");
+		$(".f3").css("color", "red");
+	});
+	$(".f4, .t4").click(function(){
+		$(".t4").css("color", "green");
+		$(".f4").css("color", "red");
+	});
+	$(".f5, .t5").click(function(){
+		$(".t5").css("color", "green");
+		$(".f5").css("color", "red");
+	});
+	$(".f6, .t6").click(function(){
+		$(".t6").css("color", "green");
+		$(".f6").css("color", "red");
+	});
+	$(".f7, .t7").click(function(){
+		$(".t7").css("color", "green");
+		$(".f7").css("color", "red");
+	});
+	$(".f8, .t8").click(function(){
+		$(".t8").css("color", "green");
+		$(".f8").css("color", "red");
+	});
+	$(".f9, .t9").click(function(){
+		$(".t9").css("color", "green");
+		$(".f9").css("color", "red");
+	});
+	$(".f10, .t10").click(function(){
+		$(".t10").css("color", "green");
+		$(".f10").css("color", "red");
 	});
 });
